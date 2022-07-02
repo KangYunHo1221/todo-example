7604577b35471a0824138aba7cd380c22056323a
Yunho <ansghkdbsgh@gmail.com>
2022-07-02 19:11:32 +0900

    feat: inprogress

--- src/components/CountIndicator.tsx
+++ src/components/CountIndicator.tsx
@@ -1,4 +1,11 @@
+import axios from 'axios';
+import { useMyQuery } from 'myQuery/useMyQuery';
+
 const CountIndicator = ({ completedCount }: { completedCount: number }) => {
+  const { response, isLoading, error } = useMyQuery('todos', () =>
+    axios.get('http://localhost:4000/todos').then((res) => res.data),
+  );
+
   return <div>Total completed todo : {completedCount}</div>;
 };
 
